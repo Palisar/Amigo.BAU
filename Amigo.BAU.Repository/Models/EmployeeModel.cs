a9a676d01029a1346cc30767c3c697d17b0975a7
Palisar <Palisar@hotmail.co.uk>
2022-03-23 13:40:57 +0000

    Altered employee model

---
+++ Amigo.BAU.Repository/Models/EmployeeModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Amigo.BAU.Repository.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
