9b40dee744502f0990f652a0727fed44b1ae5d23
Paul Keating <palisar@hotmail.co.uk>
2022-04-03 16:38:24 +0100

    Created a core project and moved my data models into them

---
+++ Amigo.BAU.Core/Models/EngineerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amigo.BAU.Repository.ViewModels
+{
+    public class EngineerViewModel
+    {
+        public string Name { get; set; }
+        public int EngineerId { get; set; }
+        public DateTimeOffset FirstShift { get; set; }
+        public DateTimeOffset LastShift { get; set; }
+        public int ShiftCount { get; set; }
+    }
+}
