68cf4543ed7b7d9d9abab422bed4912c4c079097
Paul Keating <palisar@hotmail.co.uk>
2022-04-03 21:30:20 +0100

    Added employee controller

---
+++ Amigo.BAU.API/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Amigo.BAU.Persistance.Models;
+using Amigo.BAU.Repository.EmployeeRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amigo.BAU.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _repo;
+
+        public EmployeeController(IEmployeeRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            employee.EmployeeId = _repo.GetAll().Select(x => x.EmployeeId).Max() + 1;
+            _repo.Add(employee);
+            
+            return Ok(employee);
+        }
+    }
+}
