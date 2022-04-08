7a9cdec830ecfeb2dcb32c521af9f086f7c21a52
Palisar <Palisar@hotmail.co.uk>
2022-04-08 20:23:11 +0100

    Added unit of work and interface

---
+++ Amigo.BAU.Repository/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amigo.BAU.Repository.EngineerRepository;
+
+namespace Amigo.BAU.Repository.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IEngineerRepository EngineerRepository { get; }
+        IDbConnection Connection { get; }
+        IDbTransaction Transaction { get; }
+        void Begin();
+        Task BeginAsync();
+        void Commit();
+        Task CommitAsync();
+        void Dispose();
+        void RollBack();
+        Task RollBackAsync();
+    }
+}
---
+++ Amigo.BAU.Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amigo.BAU.Repository.EngineerRepository;
+using Amigo.BAU.Repository.Interfaces;
+
+namespace Amigo.BAU.Repository.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbConnection _connection;
+        private readonly IEngineerRepository _repository;
+        private DbTransaction _transaction;
+
+        public UnitOfWork(DbConnection connection, IEngineerRepository repository)
+        {
+            _connection = connection;
+            _repository = repository;
+        }
+
+        public IEngineerRepository EngineerRepository => _repository;
+        public IDbConnection Connection => _connection;
+        public IDbTransaction Transaction => _transaction;
+        public void Begin()
+        {
+            _transaction = _connection.BeginTransaction();
+        }
+
+        public async Task BeginAsync()
+        {
+            _transaction =  await _connection.BeginTransactionAsync();
+        }
+
+        public void Commit()
+        {
+            _transaction.Commit();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _transaction.CommitAsync();
+        }
+
+        public void Dispose()
+        {
+            if (_transaction != null)
+            {
+                _transaction.Dispose();
+            }
+            _transaction = null;
+        }
+
+        public void RollBack()
+        {
+            _transaction.Rollback();
+        }
+
+        public async Task RollBackAsync()
+        {
+            await _transaction.RollbackAsync();
+        }
+    }
+}
