509784ed0b1931364052d6f39aecf5c7adfa4496
Palisar <Palisar@hotmail.co.uk>
2022-04-08 12:24:11 +0100

    added support team function to keep track of the current days team

---
+++ AmigoBAU.Application/Interfaces/ISupportTeam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amigo.BAU.Persistance.QueryModels;
+
+namespace Amigo.BAU.Application.Interfaces
+{
+    public interface ISupportTeam
+    {
+        IEnumerable<ShiftWorker> Staff { get; set; }
+    }
+}
