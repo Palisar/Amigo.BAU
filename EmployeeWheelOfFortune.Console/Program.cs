084f1d89b0d7d6f9bdf4b02bf39987c031222995
Paul Keating <83838480+Palisar@users.noreply.github.com>
2022-04-09 10:38:18 +0100

    Delete EmployeeWheelOfFortune.Console directory

--- EmployeeWheelOfFortune.Console/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using EmployeeWheelOfFortune.Console;
-
-IEmployeeRepository employeeRepository = new InMemoryEmployeeDatabase();
-WheelOfFortune wheelOfFortune = new(employeeRepository);
-var todaysEmployees = wheelOfFortune.WhoGoesToday(2);
-foreach (var employee in todaysEmployees)
-{
-    Console.WriteLine($"{employee.Name} will work today.");
-}
