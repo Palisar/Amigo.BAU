3257029644c7491e15c63f56a28a9f22111f6862
Palisar <Palisar@hotmail.co.uk>
2022-04-11 23:23:34 +0100

    removed employee repo and added async functionallity for the engineer repo

--- Amigo.BAU.Repository/EngineerRepository/EngineerRepository.cs
+++ Amigo.BAU.Repository/EngineerRepository/EngineerRepository.cs
@@ -13,52 +13,52 @@ namespace Amigo.BAU.Repository.EngineerRepository
         {
             _db = db;
         }
-        //TODO : Add unit of work pattern
-        public IEnumerable<Engineer> GetAll()
+
+        public async Task<IEnumerable<Engineer>> GetAll()
         {
-           return _db.Query<Engineer>("SELECT * FROM Engineers");
+           return await _db.QueryAsync<Engineer>("SELECT * FROM Engineers");
         }
 
-        public IEnumerable<ShiftWorker> GetNamedEngineers()
+        public async Task<IEnumerable<ShiftWorker>> GetNamedEngineers()
         {
-            return _db.Query<ShiftWorker>("SELECT * FROM Engineers INNER JOIN Employees ON Engineers.EmployeeId = Employees.EmployeeId;");
+            return await _db.QueryAsync<ShiftWorker>("SELECT * FROM Engineers INNER JOIN Employees ON Engineers.EmployeeId = Employees.EmployeeId;");
         }
 
-        public Engineer GetById(int id)
+        public async Task<Engineer> GetById(int id)
         {
-            var engineer = _db.Query<Engineer>("SELECT * FROM Engineers WHERE Id = @id", new { id }).FirstOrDefault();
+            var engineer = await _db.QuerySingleAsync<Engineer>("SELECT * FROM Engineers WHERE Id = @id", new { id });
             return engineer;
         }
-        public Engineer Add(Engineer entity)
+        public async Task<Engineer> Add(Engineer entity)
         {
-            entity.EngineerId = _db.QueryFirstAsync("SELECT MAX(EngineerId) FROM Engineers").Result + 1;
-            _db.Execute("INSERT INTO Engineers (EngineerId, EmployeeId) VALUES (@EngineerId, @EmployeeId)", entity);
+            entity.EngineerId = await _db.QueryFirstAsync("SELECT MAX(EngineerId) FROM Engineers").Result + 1;
+            await _db.ExecuteAsync("INSERT INTO Engineers (EngineerId, EmployeeId) VALUES (@EngineerId, @EmployeeId)", entity);
             return entity;
         }
 
-        public void Update(Engineer entity, int id)
+        public async Task UpdateAll(IEnumerable<Engineer> workers)
+        {
+           var query =
+                @"UPDATE Engineers SET EngineerId = @EngineerId, FirstShift = @FirstShift, LastShift = @LastShift, ShiftCount = @ShiftCount, @EmployeeId = EmployeeId WHERE EngineerId = @EngineerId";
+            foreach (var shiftWorker in workers)
+            {
+                await _db.ExecuteAsync(query, shiftWorker);
+            }
+        }
+        public async Task Update(Engineer entity, int id)
         {
             var query =
                 @"UPDATE Engineers SET EngineerId = @EngineerId, FirstShift = @FirstShift, LastShift = @LastShift, ShiftCount = @ShiftCount, @EmployeeId = EmployeeId WHERE EngineerId = @EngineerId";
-            _db.Execute(query, entity);
-            //{
-            //    entity.EngineerId,
-            //    entity.FirstShift,
-            //    entity.LastShift,
-            //    entity.ShiftCount,
-            //    entity.EmployeeId
-            //}
-
+            await _db.ExecuteAsync(query, entity);
         }
-        public void Delete(Engineer entity)
+        public async Task Delete(Engineer entity)
         {
-            _db.Execute("DELETE FROM Engineers WHERE EngineerId = @EngineerId", entity);
+            await _db.ExecuteAsync("DELETE FROM Engineers WHERE EngineerId = @EngineerId", entity);
         }
-        public void Delete(int id)
+        public async Task Delete(int id)
         {
-            _db.Execute("DELETE FROM Engineers WHERE EngineerId = @id", new { id });
+            await _db.ExecuteAsync("DELETE FROM Engineers WHERE EngineerId = @id", new { id });
         }
-
         public void Dispose()
         {
             _db.Dispose(); 
--- Amigo.BAU.Repository/EngineerRepository/InMemoryEngineerRepository.cs
+++ Amigo.BAU.Repository/EngineerRepository/InMemoryEngineerRepository.cs
@@ -15,6 +15,7 @@ namespace Amigo.BAU.Repository.EngineerRepository
             new ShiftWorker
             {
                 Name = "Paul",
+                Email = "Paul@mail.com",
                 EngineerId = 1,
                 FirstShift = DateTimeOffset.UtcNow.AddDays(-3).Date,
                 LastShift = DateTimeOffset.UtcNow.AddDays(-3).Date,
@@ -23,6 +24,7 @@ namespace Amigo.BAU.Repository.EngineerRepository
             new ShiftWorker
             {
                 Name = "Rae",
+                Email = "Rae@mail.com",
                 EngineerId = 2,
                 FirstShift = null,
                 LastShift = null,
@@ -31,6 +33,7 @@ namespace Amigo.BAU.Repository.EngineerRepository
             new ShiftWorker
             {
                 Name = "Phil",
+                Email = "Phil@mail.com",
                 EngineerId = 3,
                 FirstShift = DateTimeOffset.UtcNow.AddDays(-6).Date,
                 LastShift = DateTimeOffset.UtcNow.AddDays(-1).Date,
@@ -39,6 +42,7 @@ namespace Amigo.BAU.Repository.EngineerRepository
             new ShiftWorker
             {
                 Name = "Rob",
+                Email = "Rob@mail.com",
                 EngineerId = 4,
                 FirstShift = DateTimeOffset.UtcNow.AddDays(-1).Date,
                 LastShift = DateTimeOffset.UtcNow.AddDays(-1).Date,
@@ -47,6 +51,7 @@ namespace Amigo.BAU.Repository.EngineerRepository
             new ShiftWorker
             {
                 Name = "Anne",
+                Email = "Anne@mail.com",
                 EngineerId = 5,
                 FirstShift = DateTimeOffset.UtcNow.AddDays(-14).Date,
                 LastShift = DateTimeOffset.UtcNow.AddDays(-3).Date,
@@ -55,6 +60,7 @@ namespace Amigo.BAU.Repository.EngineerRepository
             new ShiftWorker
             {
                 Name = "Marlon",
+                Email = "Marlon@mail.com",
                 EngineerId = 6,
                 FirstShift = DateTimeOffset.UtcNow.AddDays(-13).Date,
                 LastShift = DateTimeOffset.UtcNow.AddDays(-4).Date,
@@ -63,6 +69,7 @@ namespace Amigo.BAU.Repository.EngineerRepository
             new ShiftWorker
             {
                 Name = "Elan",
+                Email = "Elan@mail.com",
                 EngineerId = 7,
                 FirstShift = DateTimeOffset.UtcNow.AddDays(-12).Date,
                 LastShift = DateTimeOffset.UtcNow.AddDays(-7).Date,
@@ -71,6 +78,7 @@ namespace Amigo.BAU.Repository.EngineerRepository
             new ShiftWorker
             {
                 Name = "Mouse",
+                Email = "Mouse@mail.com",
                 EngineerId = 8,
                 FirstShift = null,
                 LastShift = null,
@@ -79,6 +87,7 @@ namespace Amigo.BAU.Repository.EngineerRepository
             new ShiftWorker
             {
                 Name = "Puca",
+                Email = "Puca@mail.com",
                 EngineerId = 9,
                 FirstShift = DateTimeOffset.UtcNow.AddDays(-7).Date,
                 LastShift = DateTimeOffset.UtcNow.AddDays(-4).Date,
@@ -86,6 +95,7 @@ namespace Amigo.BAU.Repository.EngineerRepository
             }, new ShiftWorker
             {
                 Name = "Manx",
+                Email = "Manx@mail.com",
                 EngineerId = 10,
                 FirstShift = DateTimeOffset.UtcNow.AddDays(-4).Date,
                 LastShift = DateTimeOffset.UtcNow.AddDays(-4).Date,
@@ -93,7 +103,7 @@ namespace Amigo.BAU.Repository.EngineerRepository
             }
         };
 
-        public Engineer GetById(int id)
+        public async Task<Engineer>GetById(int id)
         {
             var engineer = workers.FirstOrDefault(w => w.EngineerId == id);
             return new Engineer
@@ -106,13 +116,13 @@ namespace Amigo.BAU.Repository.EngineerRepository
             };
         }
 
-        public Engineer Add(Engineer entity)
+        public async Task<Engineer> Add(Engineer entity)
         {
             entity.EmployeeId = workers.Max(x => x.EngineerId) + 1;
             return entity;
         }
 
-        public void Update(Engineer entity, int id)
+        public async Task Update(Engineer entity, int id)
         {
             var eng = workers.FirstOrDefault(x => x.EngineerId == id);
             eng.FirstShift = entity.FirstShift;
@@ -120,13 +130,13 @@ namespace Amigo.BAU.Repository.EngineerRepository
             eng.ShiftCount = entity.ShiftCount;
         }
 
-        public void Delete(int id)
+        public async Task Delete(int id)
         {
             var engineer = workers.FirstOrDefault(x => x.EngineerId == id);
             workers.Remove(engineer);
         }
 
-        public IEnumerable<Engineer> GetAll()
+        public async Task<IEnumerable<Engineer>>GetAll()
         {
             var engineers = new List<Engineer>();
             foreach (var shiftWorker in workers)
@@ -143,9 +153,18 @@ namespace Amigo.BAU.Repository.EngineerRepository
             return engineers;
         }
 
-        public IEnumerable<ShiftWorker> GetNamedEngineers()
+        public async Task<IEnumerable<ShiftWorker>> GetNamedEngineers()
         {
             return workers;
+        }
+
+        public async Task UpdateAll(IEnumerable<Engineer> workers)
+        {
+            foreach (var shiftWorker in workers)
+            {
+                await Update(shiftWorker, shiftWorker.EngineerId);
+
+            }
         }
     }
 }
