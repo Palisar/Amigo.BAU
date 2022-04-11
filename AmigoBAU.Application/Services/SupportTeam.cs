653f5fea6eb0731e9d1eee98942fad10f231c822
Palisar <Palisar@hotmail.co.uk>
2022-04-11 23:25:33 +0100

    support team now nullable

---
+++ AmigoBAU.Application/Services/SupportTeam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amigo.BAU.Application.Interfaces;
+using Amigo.BAU.Persistance.QueryModels;
+
+namespace Amigo.BAU.Application.Services
+{
+    public class SupportTeam : ISupportTeam
+    {
+        public IEnumerable<ShiftWorker> Staff { get; set; }
+    }
+}
