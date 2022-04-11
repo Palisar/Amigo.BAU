043aae3f7daa31359598c7ed24f0f47e17384b4a
Palisar <Palisar@hotmail.co.uk>
2022-04-11 23:26:59 +0100

    refactored SWoF service to be more readable and testable

--- AmigoBAU.Application/Services/SupportWheelOfFateService.cs
+++ AmigoBAU.Application/Services/SupportWheelOfFateService.cs
@@ -1,8 +1,8 @@
 ﻿using Amigo.BAU.Application.Interfaces;
 using Amigo.BAU.Persistance.Models;
 using Amigo.BAU.Persistance.QueryModels;
-using Amigo.BAU.Repository.EngineerRepository;
 using Amigo.BAU.Repository.Interfaces;
+using System.Linq;
 
 namespace Amigo.BAU.Application.Services
 {
@@ -11,7 +11,6 @@ namespace Amigo.BAU.Application.Services
         private readonly Random random = new();
         private readonly IDateTimeProvider _date;
         private readonly ISupportTeam _team;
-        //private readonly IEngineerRepository _repository;
         private readonly IUnitOfWork _unitOfWork;
         private readonly int numberOfShiftWorkers;
 
@@ -23,71 +22,83 @@ namespace Amigo.BAU.Application.Services
             numberOfShiftWorkers = 2; // change this to set the number of engineers selected for support
         }
         public async Task<IEnumerable<ShiftWorker>> WhoGoesToday()
-        {
+            {
             _date.GetDay();
-            if (!_date.DayChanged)
-            {
-                return _team.Staff;
-            }
 
-            // if day has not yet passed the just return the current list of employees
             await _unitOfWork.BeginAsync();
-            var engineers = _unitOfWork.EngineerRepository.GetNamedEngineers();
+            
+            var whosUp = await GetAvailableShiftWorkersAsync();
 
-            //gets a list of workers who did work yesterday
-            var whoWorked = engineers.Where(shiftWorker => shiftWorker.LastShift == DateTimeOffset.UtcNow.AddDays(-1).Date);
-
-            //gets a list of workers who did not work
-            var repo = engineers.Where(employee => employee.LastShift != DateTimeOffset.UtcNow.AddDays(-1));
-
-            //check for anyone who might not have worked yet
-            var whosUp = repo.Where(x => x.ShiftCount < 2);
-
-            if (whosUp.ToArray().Length <= 0)
-            {
-                whosUp = repo;
-            }
-
-            //creates output array 
             var todaysEmployees = new ShiftWorker[numberOfShiftWorkers];
-
-            // TODO : use a rules engine pattern for each employee to see if they can be assigned as well once you have randomly selected them
+            
             for (int i = 0; i < numberOfShiftWorkers; i++)
             {
                 var employeeIndex = random.Next(0, whosUp.Count());
-
-                if (todaysEmployees.Contains(whosUp.ElementAt(employeeIndex)))
+                var employee = whosUp.ElementAt(employeeIndex);
+                if (todaysEmployees.Contains(employee))
                 {
                     i--;
                     continue;
                 }
 
-                var employee = whosUp.ElementAt(employeeIndex);
-                var engineerToUpdate = new Engineer
-                {
-                    EngineerId = employee.EngineerId,
-                    ShiftCount = employee.ShiftCount == null ? 1 : employee.ShiftCount+1,
-                };
-                //if (employee.FirstShift == null)
-                //{
-                //    engineerToUpdate.FirstShift = _date.GetDay();
-                //}
-                //else
-                //{
-                //    engineerToUpdate.FirstShift = employee.FirstShift;
-                //}
-                engineerToUpdate.FirstShift = employee.FirstShift == null ? _date.GetDay() : employee.FirstShift;
-
-                engineerToUpdate.LastShift = _date.GetDay();
-                _unitOfWork.EngineerRepository.Update(engineerToUpdate, engineerToUpdate.EngineerId);
                 todaysEmployees[i] = employee;
             }
 
             await _unitOfWork.CommitAsync();
             _team.Staff = todaysEmployees;
-            //await WorkerCycleCheck(todaysEmployees); TODO : need to make this its own method that takes in the whole repo after everything else is done and resets anyone whos first shift was 2 weeks ago
+            
             _unitOfWork.Dispose();
             return todaysEmployees;
+        }
+
+        private async Task<IEnumerable<ShiftWorker>> GetAvailableShiftWorkersAsync()
+        {
+            await ResetEngineers();
+            var engineers =await _unitOfWork.EngineerRepository.GetNamedEngineers();
+            
+            var repo = engineers.Where(employee => employee.LastShift != DateTimeOffset.UtcNow.AddDays(-1));
+            
+            var haventDone2 = repo.Where(x => x.ShiftCount < 2);
+
+            return haventDone2.ToArray().Length <= 0 ? repo : haventDone2;
+        }
+
+        private async Task ResetEngineers()
+        {
+            var workers = await _unitOfWork.EngineerRepository.GetAll();
+            var over2weeks = workers.Where(w => w.FirstShift > _date.GetDay().AddDays(-14).Date);
+
+            foreach (var engineer in over2weeks)
+            {
+                engineer.FirstShift = null;
+                engineer.LastShift = null;
+                engineer.ShiftCount = 0;
+            }
+
+            await _unitOfWork.EngineerRepository.UpdateAll(over2weeks);
+        }
+
+        public async Task UpdateWhoWorked()
+        {
+            _date.GetDay();
+            if (_date.DayChanged)
+            {
+                return;
+            }
+            var workers = _team.Staff;
+            foreach (ShiftWorker employee in workers)
+            {
+                var engineerToUpdate = new Engineer
+                {
+                    EngineerId = employee.EngineerId,
+                    ShiftCount = employee.ShiftCount == null ? 1 : employee.ShiftCount + 1,
+                };
+
+                engineerToUpdate.FirstShift = employee.FirstShift == null ? _date.GetDay() : employee.FirstShift;
+
+                engineerToUpdate.LastShift = _date.GetDay();
+                await _unitOfWork.EngineerRepository.Update(engineerToUpdate, engineerToUpdate.EngineerId);
+            }
         }
     }
 }
