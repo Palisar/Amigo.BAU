12e38c1cf1b427396de33166bd9a18d251d2d5d3
Palisar <Palisar@hotmail.co.uk>
2022-04-11 23:22:49 +0100

    Added refit for api to be consomed  by front end

---
+++ Amigo.BAU.Persistance/Interfaces/IAmigoBAUClient.cs
@@ -0,0 +1,11 @@
+﻿using Amigo.BAU.Persistance.QueryModels;
+using Refit;
+
+namespace Amigo.BAU.Persistance.Interfaces
+{
+    public interface IAmigoBAUClient
+    {
+        [Get("/api/WheelOfFate")]
+        Task<ApiResponse<IEnumerable<ShiftWorkerResponse>>> WheelOfFate();
+    }
+}
