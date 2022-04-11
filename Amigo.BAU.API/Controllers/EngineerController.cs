ae7b929f17875e9f5f3ed5619b94972af3b3ea23
Palisar <Palisar@hotmail.co.uk>
2022-04-11 23:17:31 +0100

    Added engineer controller

---
+++ Amigo.BAU.API/Controllers/EngineerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Amigo.BAU.API.Controllers
+{
+    public class EngineerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
