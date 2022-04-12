f14f9f51dd94416a5d5b3af75d27de581dc528bd
Palisar <Palisar@hotmail.co.uk>
2022-04-13 00:42:40 +0100

    set DI Engineer DB to in memory

--- Amigo.BAU.API/Extensions/InfrastructureExtensions.cs
+++ Amigo.BAU.API/Extensions/InfrastructureExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System.Data.Common;
-using System.Data.SqlClient;
-using Amigo.BAU.Application.Interfaces;
+﻿using Amigo.BAU.Application.Interfaces;
 using Amigo.BAU.Application.Services;
 using Amigo.BAU.Repository.EngineerRepository;
 using Amigo.BAU.Repository.Interfaces;
@@ -14,7 +12,7 @@ namespace Amigo.BAU.API.Extensions
         {
             services.AddSingleton(typeof(IDateTimeProvider), typeof(DateTimeProvider));
             services.AddSingleton(typeof(ISupportTeam), typeof(SupportTeam));
-            services.AddScoped(typeof(IEngineerRepository), typeof(EngineerRepository));
+            services.AddScoped(typeof(IEngineerRepository), typeof(InMemoryEngineerRepository));
             services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
             services.AddScoped(typeof(ISupportWheelOfFate), typeof(SupportWheelOfFateService));
         }
