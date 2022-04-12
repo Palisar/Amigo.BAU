7aec3d29a3143f3fa882e2b060acc4583b654974
Palisar <Palisar@hotmail.co.uk>
2022-04-13 00:40:54 +0100

    Added enpoint for updating the staff that returns http status message

--- Amigo.BAU.API/Controllers/EngineerController.cs
+++ Amigo.BAU.API/Controllers/EngineerController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Amigo.BAU.Application.Interfaces;
+using Amigo.BAU.Persistance.Models;
+using Amigo.BAU.Persistance.QueryModels;
+using Amigo.BAU.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Mapster;
 
 namespace Amigo.BAU.API.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class EngineerController : Controller
     {
-        public IActionResult Index()
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ISupportTeam _team;
+
+        public EngineerController(IUnitOfWork unitOfWork, ISupportTeam team)
         {
-            return View();
+            _unitOfWork = unitOfWork;
+            _team = team;
+        }
+
+        [HttpPut]
+        [Route("/updateEngineers")]
+        [ProducesResponseType(200)]
+        public async Task UpdateEngineers()
+        {
+            var staffToUpdate = _team.Staff.ToArray();
+            var mappedStaff = new Engineer[2];
+
+            for (int i = 0; i < staffToUpdate.Count(); i++)
+            {
+                mappedStaff[i] = staffToUpdate[i].Adapt<Engineer>();
+            }
+            
+            await _unitOfWork.BeginAsync();
+            await _unitOfWork.EngineerRepository.UpdateAll(mappedStaff);
+            await _unitOfWork.CommitAsync();
+            _unitOfWork.Dispose();
         }
     }
 }
