ff39e5de1370550293458b7b47bd24517592a97e
Palisar <Palisar@hotmail.co.uk>
2022-04-13 00:43:50 +0100

    Added application layer unit tests

--- Amigo.BAU.Application.Tests/DateTimeProviderTests.cs
+++ Amigo.BAU.Application.Tests/DateTimeProviderTests.cs
@@ -6,11 +6,11 @@ namespace Amigo.BAU.Application.Tests
     public class DateTimeProviderTests
     {
         DateTimeProvider sut = new();
-        
+
         [Fact]
         public void WhenServiceIsCreated_DateShouldBeYesterday()
         {
-            
+
             var result = sut.TodaysDate.Date;
             result.Should().Be(DateTimeOffset.UtcNow.AddDays(-1).Date);
         }
@@ -25,11 +25,11 @@ namespace Amigo.BAU.Application.Tests
         [Fact]
         public void IfDayDoesNotChange_Then_DayChangeIsFalse()
         {
-            
+
             sut.GetDay();
             sut.DayChanged.Should().BeTrue();
             var result = sut.GetDay();
-            
+
             result.Should().Be(DateTimeOffset.UtcNow.Date);
             sut.DayChanged.Should().BeFalse();
         }
